2d577dbd1f263018cf89f5a06a29bb4ea3238602
Nils Philippsen <nils@redhat.com>
2009-09-01 12:48:49 +0200

    update email addresses

--- slip/dbus/service.py
+++ slip/dbus/service.py
@@ -3,6 +3,7 @@
 # services
 #
 # Copyright © 2008, 2009 Red Hat, Inc.
+# Authors: Nils Philippsen <nils@redhat.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,8 +18,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-# Authors: Nils Philippsen <nphilipp@redhat.com>
 
 """This module contains convenience functions for using dbus-activated services."""
 
--- slip/util/hookable.py
+++ slip/util/hookable.py
@@ -2,7 +2,7 @@
 #
 # Copyright © 2008 Red Hat, Inc.
 # Authors:
-# Nils Philippsen <nphilipp@redhat.com>
+# Nils Philippsen <nils@redhat.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

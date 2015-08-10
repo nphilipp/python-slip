8779e8ae6b476b857f03bf348a47383704f14e74
Nils Philippsen <nils@redhat.com>
2015-08-10 22:46:00 +0200

    upload release archives to github
    (cherry picked from commit 3869ee415809cd9e48f9b87a19492bdcb2230475)

--- upload_rules.mk
+++ upload_rules.mk
@@ -1,7 +1,21 @@
 # License: GPL v2 or later
-# Copyright Red Hat Inc. 2008
+# Copyright Red Hat Inc. 2008, 2015
 
-ifdef UPLOAD_URL
+ifndef UPLOAD_METHOD
+UPLOAD_METHOD = scp
+endif
+
+PKGARCHIVE=$(PKGNAME)-$(PKGVERSION).tar.bz2
+
+ifeq ($(UPLOAD_METHOD),scp)
+ifdef UNSTABLE
+
+upload:
+	@echo Use of target \'$@\' not possible in unstable branch. >&2
+	@exit 1
+
+else ifdef UPLOAD_URL
+
 upload:
 	@url="$(UPLOAD_URL)"; \
 	case "$$url" in \
@@ -17,4 +31,17 @@ upload:
 		exit 1; \
 		;; \
 	esac
+
+else
+
+upload:
+	@echo "UPLOAD_URL undefined!" >&2
+	@false
+
+endif
+else ifeq ($(UPLOAD_METHOD),github)
+
+include github_rules.mk
+upload:	github-upload
+
 endif
